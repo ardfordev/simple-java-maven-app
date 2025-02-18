1950010029ba792aacfd8cfcbd50d458148d2f75
ardfordev <ardfordev@gmail.com>
2025-02-19 00:12:34 +0700

    edit apptest

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,22 @@
+package com.mycompany.app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class AppTest {
+    @Test
+    public void testAppConstructor() {
+        App app = new App();
+        assertNotNull(app);
+    }
+
+    @Test
+    public void testAppMessage() {
+        App app = new App();
+        String message = app.getMessage();
+        assertNotNull(message);
+        assertEquals("Hello World!\nSubmission Proyek Membangun CI/CD Pipeline dengan Jenkins\nardi_supriyadi", message);
+    }
+}
+
